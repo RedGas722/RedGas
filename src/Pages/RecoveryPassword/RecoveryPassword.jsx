310588f9f34612724717d63bb1bd35b5513d2ef1
samuraypro7767 <samuraypro7767@gmail.com>
2025-05-06 17:00:49 -0500

    feat: Add confirm password field to RecoveryPassword component; implement visibility toggle

--- src/Pages/RecoveryPassword/RecoveryPassword.jsx
+++ src/Pages/RecoveryPassword/RecoveryPassword.jsx
@@ -5,10 +5,14 @@ import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"
 
 export const RecoveryPassword = () => {
     const [showPassword, setShowPassword] = useState(false)
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword)
     }
+    const toggleConfirmPasswordVisibility = () => {
+        setShowConfirmPassword(!showConfirmPassword)
+    };
 
     return (
         <section className="w-full gap-[40px] h-dvh flex justify-center items-center">
@@ -16,6 +20,7 @@ export const RecoveryPassword = () => {
             <div className="divForm shadow_box_RL bg-glass-total rounded-3xl flex flex-col w-fit justify-self-center gap-[40px]">
                 <h1 className="text-center text-white text-4xl">¡Recuperación Contraseña!</h1>
                 <form className="flex flex-col gap-[15px] text-start w-full">
+                    {/* Password */}
                     <label htmlFor="password" className="text-white text-2xl w-full">
                         Contraseña
                     </label>
@@ -30,6 +35,23 @@ export const RecoveryPassword = () => {
                             icon={showPassword ? faEyeSlash : faEye}
                             className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer"
                             onClick={togglePasswordVisibility}
+                        />
+                    </div>
+                    {/* Confirm Password */}
+                    <label htmlFor="passwordConfirm" className="text-white text-2xl w-full">
+                        Confirmar contraseña
+                    </label>
+                    <div className="relative w-full">
+                        <input
+                            type={showConfirmPassword ? "text" : "password"}
+                            placeholder={showConfirmPassword ? "Confirmar" : "*********"}
+                            id="passwordConfirm"
+                            className="border-t-0 border-b-[1px] w-full placeholder:text-gray-400 text-gray-200 border-gray-300 outline-0"
+                        />
+                        <FontAwesomeIcon
+                            icon={showConfirmPassword ? faEyeSlash : faEye}
+                            className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer"
+                            onClick={toggleConfirmPasswordVisibility}
                         />
                     </div>
                 </form>
