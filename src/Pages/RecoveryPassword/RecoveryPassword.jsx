8abb9b9ad2eccbae654f1de04db7f9cfb9c955af
samuraypro7767 <samuraypro7767@gmail.com>
2025-05-06 16:52:01 -0500

    feat: Update layout of RecoveryPassword component; adjust form alignment and heading formatting

--- src/Pages/RecoveryPassword/RecoveryPassword.jsx
+++ src/Pages/RecoveryPassword/RecoveryPassword.jsx
@@ -13,9 +13,9 @@ export const RecoveryPassword = () => {
     return (
         <section className="w-full gap-[40px] h-dvh flex justify-center items-center">
             <Circles styleC1="right-[50%] bottom-[0px]" styleC2="left-[54%] top-[120px]" styleC3="top-[400px] left-[80px]" />
-            <div className="divForm shadow_box_RL bg-glass-total rounded-3xl flex flex-col items-center w-fit justify-self-center gap-[40px]">
-                <h1 className="text-center text-white text-4xl">Recuperación Contraseña!</h1>
-                <form className="flex flex-col gap-[15px] justify-center items-center text-start w-fit">
+            <div className="divForm shadow_box_RL bg-glass-total rounded-3xl flex flex-col w-fit justify-self-center gap-[40px]">
+                <h1 className="text-center text-white text-4xl">¡Recuperación Contraseña!</h1>
+                <form className="flex flex-col gap-[15px] text-start w-full">
                     <label htmlFor="password" className="text-white text-2xl w-full">
                         Contraseña
                     </label>
