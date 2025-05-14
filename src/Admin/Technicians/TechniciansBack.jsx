9da31b3d3f1a7c3c1d4469c9b609bc6743bfdd42
Johan Villanueva <jvillanuevagallego@gmail.com>
2025-05-14 09:58:30 -0500

    feat: Add Technicians component and associated styles; refactor TechniciansBack component

--- src/Admin/Technicians/TechniciansBack.jsx
+++ src/Admin/Technicians/TechniciansBack.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { RegisterModal } from './Register/RegisterModal';
 import { GetModal } from './Get/GetModal';
 import { DeleteModal } from './Delete/DeleteModal';
 import { UpdateModal } from './Update/UpdateModal';
-import ButtonBack from '../UI/ButtonBack/ButtonBack';
+import { ButtonBack } from '../UI/ButtonBack/ButtonBack';
 
 
 export const TechniciansBack = () => {
@@ -15,17 +15,17 @@ export const TechniciansBack = () => {
 
   return (
     <div className="p-[20px_0_0_20px] flex items-start gap-[20px] justify-start h-screen">
-        <div className='flex items-center gap-[20px] justify-center'>
-          <h1 className='font-bold text-[20px]'>Tecnico BACK-OFFICE</h1>
-          <ButtonBack ClickMod={() => setShowRegisterModal(true)} Child='Registrar' />
-          <ButtonBack ClickMod={() => setShowGetModal(true)} Child='Consultar' />
-          <ButtonBack ClickMod={() => setShowUpdateModal(true)} Child='Actualizar' />
-          <ButtonBack ClickMod={() => setShowDeleteModal(true)} Child='Eliminar' />
-        </div>
+      <div className='flex items-center gap-[20px] justify-center'>
+        <h1 className='font-bold text-[20px]'>Tecnico BACK-OFFICE</h1>
+        <ButtonBack ClickMod={() => setShowRegisterModal(true)} Child='Registrar' />
+        <ButtonBack ClickMod={() => setShowGetModal(true)} Child='Consultar' />
+        <ButtonBack ClickMod={() => setShowUpdateModal(true)} Child='Actualizar' />
+        <ButtonBack ClickMod={() => setShowDeleteModal(true)} Child='Eliminar' />
+      </div>
 
       {showRegisterModal && (
         <RegisterModal onClose={() => setShowRegisterModal(false)} />
-      )}  
+      )}
 
       {showGetModal && (
         <GetModal onClose={() => setShowGetModal(false)} />
