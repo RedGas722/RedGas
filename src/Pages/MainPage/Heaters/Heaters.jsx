4a3ad2e842649ad33d4baedfead2c0e075500bd5
Johan Villanueva <jvillanuevagallego@gmail.com>
2025-05-17 13:57:48 -0500

    feat: Refactor Cards and Offers components for improved structure and styling

--- src/Pages/MainPage/Heaters/Heaters.jsx
+++ src/Pages/MainPage/Heaters/Heaters.jsx
@@ -1,31 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { faWater } from '@fortawesome/free-solid-svg-icons';
 import SectCategory from '../../../UI/TitleSectCategory/TitleSectCategory';
 import Cards from '../../../UI/Cards/Cards';
 
 export const HeatersSect = () => {
-  const [calentadores, setCalentadores] = useState([]);
-
-  useEffect(() => {
-    async function fetchCalentadores() {
-      try {
-        const res = await fetch('http://localhost:10101/ProductoGetAll'); 
-        if (!res.ok) throw new Error('Error al obtener productos');
-        const data = await res.json();
-        setCalentadores(data.data || []); // data.data depende de cómo tu API responda
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchCalentadores();
-  }, []);
-
-  return (
-    <section id="HeatersSect" className="flex flex-col gap-[20px]">
-      <SectCategory iconCategory={faWater} nameCategory="Calentadores" className='flex flex-row justify-start items-center gap-[8px]' />
-      <Cards uniqueId="heaters" productos={calentadores} />
-    </section>
-  );
+    return (
+        <section id="HeatersSect" className="flex flex-col gap-[20px]">
+            <SectCategory iconCategory={faWater} nameCategory="Calentadores" className='flex flex-row justify-start items-center gap-[8px]' />
+            <Cards uniqueId="heaters" titleCatt="Calentador" Price="999999" brandCatt="Oka" imgContent="https://armogas.com/wp-content/uploads/2024/06/calentador-8l-tn-mecanico-rheem-img-01.jpg"/>
+        </section>
+    );
 };
 
 export default HeatersSect;
