9a1556c7b8249cc4ddfe64482907c57e6eb2d3c1
Johan Villanueva <jvillanuevagallego@gmail.com>
2025-05-17 17:49:10 -0500

    refactor: Update HeatersSect component structure and styling

--- src/Pages/MainPage/Heaters/Heaters.jsx
+++ src/Pages/MainPage/Heaters/Heaters.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { faWater } from '@fortawesome/free-solid-svg-icons';
 import { TitleSectCategory } from '../../../UI/TitleSectCategory/TitleSectCategory'
 import { Cards } from '../../../UI/Cards/Cards'
@@ -8,10 +9,10 @@ export const HeatersSect = () => {
   useEffect(() => {
     async function fetchCalentadores() {
       try {
-        const res = await fetch('http://localhost:10101/ProductoGetAll'); 
+        const res = await fetch('http://localhost:10101/ProductoGetAll');
         if (!res.ok) throw new Error('Error al obtener productos');
         const data = await res.json();
-        setCalentadores(data.data || []); 
+        setCalentadores(data.data || []);
       } catch (error) {
         console.error(error);
       }
@@ -20,17 +21,11 @@ export const HeatersSect = () => {
   }, []);
 
   return (
-    <section id="HeatersSect" className="flex flex-col gap-[20px]">
-      <SectCategory iconCategory={faWater} nameCategory="Calentadores" className='flex flex-row justify-start items-center gap-[8px]' />
-      <Cards uniqueId="heaters" productos={calentadores} />
+    <section id="HeatersSect" className="NeoContainer_outset_TL flex flex-col p-[15px] gap-[20px]">
+      <TitleSectCategory iconCategory={faWater} nameCategory="Calentadores" className='flex flex-row justify-start items-center gap-[8px]' />
+      <Cards uniqueId="heaters" titleCatt="Calentador"  productos={calentadores} Price="999999" brandCatt="Oka" imgContent="https://armogas.com/wp-content/uploads/2024/06/calentador-8l-tn-mecanico-rheem-img-01.jpg" />
     </section>
   );
-    return (
-        <section id="HeatersSect" className="NeoContainer_outset_TL flex flex-col p-[15px] gap-[20px]">
-            <TitleSectCategory iconCategory={faWater} nameCategory="Calentadores" className='flex flex-row justify-start items-center gap-[8px]' />
-            <Cards uniqueId="heaters" titleCatt="Calentador" Price="999999" brandCatt="Oka" imgContent="https://armogas.com/wp-content/uploads/2024/06/calentador-8l-tn-mecanico-rheem-img-01.jpg" />
-        </section>
-    );
 };
 
 export default HeatersSect;
