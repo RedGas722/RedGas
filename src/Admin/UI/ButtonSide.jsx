b36b70efaa7867263193bd62bafcd9613f834b6f
Johan Villanueva <jvillanuevagallego@gmail.com>
2025-05-26 23:16:44 -0500

    fix: Update ButtonSide component to add gap between NavLink elements for improved layout

--- src/Admin/UI/ButtonSide.jsx
+++ src/Admin/UI/ButtonSide.jsx
@@ -4,7 +4,7 @@ import './ButtonSide.css'
 export const ButtonSide = ({ to, children, imgBtn }) => {
     return (
         <div>
-            <NavLink to={to} className="hover:text-[var(--Font-Nav)] hover:font-bold text-[var(--main-color)] w-full flex flex-col items-center">
+            <NavLink to={to} className="hover:text-[var(--Font-Nav)] gap-2 text-[var(--main-color)] w-full flex flex-col items-center">
                 <button className="NavsBTN p-[16px_0] NeoContainer_Admin_outset_TL h-26 w-26 flex justify-center items-center cursor-pointer">
                     <img src={imgBtn} alt={children} className='w-20' />
                 </button>
