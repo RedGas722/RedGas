fcf73088b94528f31aebfd6a944a5b4369aa0e6a
David Steven T <davidtoro013125@gmail,com>
2025-05-28 13:05:19 -0500

    new branch to work on

---
+++ src/Pages/Costumer/CostumerServices.jsx
@@ -0,0 +1,12 @@
+// import './TechniciansPage.css'
+// import usuarios from "./TechniciansArray"
+
+export const CostumerServices = () => {
+    return (
+        <section className="h-fit MainPageContainer flex flex-wrap justify-center justify-self-center self-center items-center gap-[20px]">
+            hola
+        </section>
+    )
+}
+
+export default CostumerServices
