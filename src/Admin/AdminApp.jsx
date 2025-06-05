414e78bd3efc57a1ce7c480a1fb8c92f6ab76632
samuraypro7767 <samuraypro7767@gmail.com>
2025-06-05 17:13:25 -0500

    Rename ButtonSide component to ButtonAdmin for clarity and update image paths for consistency

--- src/Admin/AdminApp.jsx
+++ src/Admin/AdminApp.jsx
@@ -7,15 +7,15 @@ export const AdminApp = () => {
             <h2 className='font-bold text-4xl text-[var(--Font-Nav)] fixed top-5 left-5'>Admin Page</h2>
             <div className="Admin p-[2%_0_2%_1%] w-dvw h-dvh bg-[var(--Bacground-Admin)] flex items-center justify-center">
                 <section id="sideBarr" className="h-fit p-[0_30px] gap-10 justify-center justify-self-center self-center flex flex-wrap">
-                    <ButtonSide to='/Admin/Technicians' children='Técnicos' imgBtn='../../src/Admin/Assets/Icons/Tecnic.png' />
-                    <ButtonSide to='/Admin/Clients' children='Clientes' imgBtn='../../src/Admin/Assets/Icons/Client.png' />
-                    <ButtonSide to='/Admin/Employees' children='Empleados' imgBtn='../../src/Admin/Assets/Icons/Employer.png' />
-                    <ButtonSide to='/Admin/Products' children='Productos' imgBtn='../../src/Admin/Assets/Icons/Product.png' />
-                    <ButtonSide to='/Admin/Factures' children='Facturas' imgBtn='../../src/Admin/Assets/Icons/Facture.png' />
-                    <ButtonSide to='/Admin/Categories' children='Categorias' imgBtn='../../src/Admin/Assets/Icons/Categories.png' />
-                    <ButtonSide to='/Admin/Services' children='Servicios' imgBtn='../../src/Admin/Assets/Icons/Services.png' />
-                    <ButtonSide to='/Admin/Admins' children='Admin' imgBtn='../../src/Admin/Assets/Icons/Admin.png' />
-                    <ButtonSide to='/Admin/Contracts' children='Contratos' imgBtn='../../src/Admin/Assets/Icons/Contract.png' />
+                    <ButtonSide to='/Admin/Technicians' children='Técnicos' imgBtn='src/Admin/Assets/Icons/Tecnic.png' />
+                    <ButtonSide to='/Admin/Clients' children='Clientes' imgBtn='src/Admin/Assets/Icons/Client.png' />
+                    <ButtonSide to='/Admin/Employees' children='Empleados' imgBtn='src/Admin/Assets/Icons/Employer.png' />
+                    <ButtonSide to='/Admin/Products' children='Productos' imgBtn='src/Admin/Assets/Icons/Product.png' />
+                    <ButtonSide to='/Admin/Factures' children='Facturas' imgBtn='src/Admin/Assets/Icons/Facture.png' />
+                    <ButtonSide to='/Admin/Categories' children='Categorias' imgBtn='src/Admin/Assets/Icons/Categories.png' />
+                    <ButtonSide to='/Admin/Services' children='Servicios' imgBtn='src/Admin/Assets/Icons/Services.png' />
+                    <ButtonSide to='/Admin/Admins' children='Admin' imgBtn='src/Admin/Assets/Icons/Admin.png' />
+                    <ButtonSide to='/Admin/Contracts' children='Contratos' imgBtn='src/Admin/Assets/Icons/Contract.png' />
                 </section>
             </div>
         </>
