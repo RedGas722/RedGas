38a818210219b3b4742611930344bf4a41e13a25
Aprendiz <lukitor1414@gmail.com>
2025-06-18 12:35:34 -0500

    pull e instalacion de dependencias para la rama

--- src/Pages/Login/TokenRefresher.jsx
+++ src/Pages/Login/TokenRefresher.jsx
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-export function startTokenRefresher(remember = false) {
-    // 10 minutos -> 600000 ms
-    const intervalTime = 600000;
-    const token = localStorage.getItem('token');
-
-    if (!token) {
-        console.warn('No se encontró token para renovar');
-        clearInterval(interval);
-        localStorage.removeItem('tipo_usuario');
-        return;
-    }
-
-
-    const interval = setInterval(async () => {
-
-        if (remember === false) {
-            try {
-                const response = await fetch('https://redgas.onrender.com/renewToken', {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                });
-
-                if (response.ok) {
-                    const data = await response.json();
-                    localStorage.setItem('token', data.token);
-                    console.log('✅ Token renovado exitosamente');
-                } else {
-                    console.warn('No se pudo renovar el token, cerrando sesión');
-                    clearInterval(interval);
-                    localStorage.removeItem('token');
-                    localStorage.removeItem('tipo_usuario');
-                }
-            } catch (error) {
-                console.error('Error al renovar token:', error);
-                clearInterval(interval);
-                localStorage.removeItem('token');
-                localStorage.removeItem('tipo_usuario');
-            }
-        } else if (remember === true) {
-            try {
-                const response = await fetch('https://redgas.onrender.com/renewToken', {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                });
-            } catch (error) {
-                console.error('Error al renovar token:', error);
-            }
-=======
 import Swal from 'sweetalert2';
 
 export function startTokenRefresher() {
@@ -92,7 +39,6 @@ export function startTokenRefresher() {
                 handleSessionExpired("Tu sesión ha expirado por inactividad. Serás redirigido a la página principal.");
                 return;
             }
->>>>>>> 2e5fccc042350fca7bf89db615d1ab55ba392b4b
         }
 
         // Intentar renovar token
@@ -117,10 +63,6 @@ export function startTokenRefresher() {
 
     }, intervalTime);
 
-<<<<<<< HEAD
-    return interval;
-}
-=======
     const handleSessionExpired = (message) => {
         clearInterval(interval);
         localStorage.removeItem('token');
@@ -141,4 +83,3 @@ export function startTokenRefresher() {
 
     return interval;
 }
->>>>>>> 2e5fccc042350fca7bf89db615d1ab55ba392b4b
