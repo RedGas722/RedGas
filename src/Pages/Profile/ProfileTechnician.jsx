476074747c651b304bd6f9ccc5df94a8c5e860fb
Johan Villanueva <jvillanuevagallego@gmail.com>
2025-07-11 00:25:24 -0500

    feat: Enhance ProfileTechnician UI with additional icons and improved layout

--- src/Pages/Profile/ProfileTechnician.jsx
+++ src/Pages/Profile/ProfileTechnician.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import BtnBack from "../../UI/Login_Register/BtnBack";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserGear } from '@fortawesome/free-solid-svg-icons';
+import { faUserGear, faIdCard, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import Buttons from "../../UI/Login_Register/Buttons";
 import { useNavigate } from "react-router-dom";
 import bcrypt from 'bcryptjs';
@@ -106,43 +106,37 @@ const ProfileTechnician = () => {
 
   return (
     <div>
-      <div className="flex z-[2] p-[0_5px] items-center justify-between ">
-        <div className="btnDown">
-          <BtnBack To='/' />
-        </div>
-        <div>
-          <h2 className=" font-bold text-4xl text-[var(--Font-Nav)]">MI PERFIL</h2>
-        </div>
+      <div className="z-[2] flex p-[5px] flex-col text-center gap-2 sm:gap-0 sm:flex-row sm:justify-between items-center w-full">
+        <BtnBack To='/' />
+        <h2 className="font-bold text-3xl sm:text-4xl text-[var(--Font-Nav)]">Mi perfil</h2>
       </div>
 
-      <section className="h-fit z-[2] flex flex-wrap justify-center text-[var(--main-color)] items-center gap-[20px] p-20">
-        <div className="flex flex-col z-[2] flex-wrap justify-center max-w-[700px] min-w-0 NeoContainer_outset_TL p-5 gap-6">
+      <section className="h-fit z-[2] flex flex-wrap justify-center text-[var(--main-color)] items-center gap-[20px] MainPageContainer">
+        <div className="flex flex-col z-[2] flex-wrap items-center justify-center max-w-[700px] min-w-0 NeoContainer_outset_TL p-5 gap-6">
 
-          <div className="text-[var(--main-color-sub)] pl-2 gap-3 flex items-center font-bold w-fit">
-            <FontAwesomeIcon icon={faUserGear} className="text-[var(--main-color)] text-5xl" />
+          <div className="text-[var(--main-color-sub)] pl-2 gap-3 justify-center flex-wrap flex items-center font-bold w-fit">
+            {tecnico.imagen && (
+              <div className="flex justify-center mt-4">
+                <img
+                  src={`data:image/jpeg;base64,${tecnico.imagen}`}
+                  alt="Foto del técnico"
+                  className="w-30 h-30 object-cover rounded-full "
+                />
+              </div>
+            )}
             <div className="flex flex-col justify-center font-light leading-[20px] gap-[2px]">
               <p className="text-xl font-bold text-[var(--main-color)]">{tecnico.nombre_tecnico}</p>
-              <p className="text-[1rem]">🆔 CC: {tecnico.cc_tecnico}</p>
-              <p className="text-[1rem]">📧 {tecnico.correo_tecnico}</p>
-              <p className="text-[1rem]">📞 {tecnico.telefono_tecnico}</p>
+              <p className="text-[1rem] flex items-center gap-2"><FontAwesomeIcon icon={faIdCard} className="text-[var(--main-color)]" /> CC: {tecnico.cc_tecnico}</p>
+              <p className="text-[1rem] flex items-center gap-2"><FontAwesomeIcon icon={faEnvelope} className="text-[var(--main-color)]" /> {tecnico.correo_tecnico}</p>
+              <p className="text-[1rem] flex items-center gap-2"><FontAwesomeIcon icon={faPhone} className="text-[var(--main-color)]" /> {tecnico.telefono_tecnico}</p>
             </div>
-            <Buttons
-              type="button"
-              nameButton="Cambiar Contraseña"
-              Onclick={handleCambiarContrasena}
-              className="bg-red-700 hover:bg-red-800 text-white font-semibold py-2 px-4 rounded-xl shadow-md transition duration-300"
-            />
           </div>
-
-          {tecnico.imagen && (
-            <div className="flex justify-center mt-4">
-              <img
-                src={`data:image/jpeg;base64,${tecnico.imagen}`}
-                alt="Foto del técnico"
-                className="w-40 h-40 object-cover rounded-full border-4 border-[var(--main-color)]"
-              />
-            </div>
-          )}
+          <Buttons
+            type="button"
+            nameButton="Cambiar Contraseña"
+            Onclick={handleCambiarContrasena}
+            className="bg-red-700 hover:bg-red-800 text-white font-semibold py-2 px-4 rounded-xl shadow-md transition duration-300"
+          />
 
         </div>
       </section>
